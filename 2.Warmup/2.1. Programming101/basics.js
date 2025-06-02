d3ff515641c8ef7e8d2c95ad5bdde96207989a06
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-02 22:29:26 +0530

    feat(Programming101): Created basic programs in both JS and python.

---
+++ 2.Warmup/2.1. Programming101/basics.js
@@ -0,0 +1,13 @@
+const firstName = 'Sandeep'
+const lastName = 'Reddy'
+console.log(firstName +" " + lastName)
+const age = 27
+console.log(27)
+const skills = ["React JS","Node JS","Angular","MongoDB","Performance optimization"]
+console.log(skills)
+const finalData = {
+    fullName : firstName +" " + lastName,
+    age : age,
+    skills : skills
+}
+console.log(finalData)
---
+++ 2.Warmup/2.1. Programming101/basics.py
@@ -0,0 +1,13 @@
+first_name = "Sandeep"
+last_name = 'Reddy'
+print(f"{first_name} {last_name}")
+age = 27
+print(age)
+skills = ["React JS","Node JS","Angular","MongoDB","Performance optimization"]
+print(skills)
+final_data = {
+    'full_name' : first_name +" " + last_name,
+    'age' : age,
+    'skills' : skills
+}
+print(final_data)
