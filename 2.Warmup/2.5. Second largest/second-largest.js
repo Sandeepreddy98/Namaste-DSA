5d4109f448af8449463d10dc0238f1dd17512483
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-09 15:38:59 +0530

    feat(Second Largest): Covered corner cases
    1. If array is empty. 2. If array has only 1 element. 3)If largest number have duplicates in the array.

--- 2.Warmup/2.5. Second largest/second-largest.js
+++ 2.Warmup/2.5. Second largest/second-largest.js
@@ -11,13 +11,16 @@ console.log(secondLargestUsingSort([1,-9,2,8,3,-7,4,0,-4]))
 
 // Create a function to find the second largest without sorting
 function secondLargest(arr){
+    if(arr.length < 2){
+        return "Array should have atleast 2 elements"
+    }
     let firstLargest = arr[0]
     let secondLargestEle = arr[0]
     for(let i=1;i<arr.length;i++){
         if(arr[i] > firstLargest){
             secondLargestEle = firstLargest
             firstLargest = arr[i]
-        }else if (arr[i] > secondLargestEle){
+        }else if (arr[i] > secondLargestEle && arr[i] != firstLargest){
             secondLargestEle = arr[i]
         }
     }
