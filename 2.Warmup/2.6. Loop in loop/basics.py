cb287a503885d8f3e74cdf2001ab15ff92bf7812
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-12 23:02:17 +0530

    feat(2.6 Loop in loop): Created a function Nested Loop with condition j <= i in python

--- 2.Warmup/2.6. Loop in loop/basics.py
+++ 2.Warmup/2.6. Loop in loop/basics.py
@@ -15,3 +15,12 @@ def conditionalNestedLoop(x,y):
             print(i,j)
         
 conditionalNestedLoop(3,3)
+
+# Nested loop with condition j <= i
+print("------ Nested Loop with condition j <= i---------")
+def conditionalNestedLoop2(x,y):
+    for i in range(0,x):
+        for j in range(0,i+1):
+            print(i,j)
+        
+conditionalNestedLoop2(3,3)
