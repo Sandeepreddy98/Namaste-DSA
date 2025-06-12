0e82d7fd61e91362393ced267003d698810f5edb
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-12 22:53:26 +0530

    feat(2.6 Loop in loop): created a function with Nested loop with condition j < i

--- 2.Warmup/2.6. Loop in loop/basics.js
+++ 2.Warmup/2.6. Loop in loop/basics.js
@@ -9,3 +9,15 @@ function nestedLoop(x,y){
 }
 
 nestedLoop(3,3)
+
+//Nested loop with condition j < i
+console.log("------ Nested Loop with condition j < i---------")
+function conditionalNestedLoop(x,y){
+    for(let i=0;i<x;i++){
+        for(let j =0;j<i;j++){
+            console.log(i,j)
+        }
+    }
+}
+
+conditionalNestedLoop(3,3)
