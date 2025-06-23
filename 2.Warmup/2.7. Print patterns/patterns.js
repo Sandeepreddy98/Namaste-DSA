9dd684b4c7a9a118f7cd40f1dd35ecac1741dcff
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-23 22:48:36 +0530

    feat(2.7 Star Patterns): Number Pattern - Right Angled Incremental Y-axis.

--- 2.Warmup/2.7. Print patterns/patterns.js
+++ 2.Warmup/2.7. Print patterns/patterns.js
@@ -31,3 +31,37 @@ function rightTrianglePattern(n){
 }
 
 console.log(rightTrianglePattern(4))
+
+// Number Pattern - Right Angled Increment
+console.log("------ Number Pattern - Right Angled Increment ---------")
+
+function rightAngledNumberIncrement(n){
+    let pattern = ''
+    for(let i = 1;i<= n;i++){
+        let row = ''
+        for (let j=1;j<=i;j++){
+            row += j
+        }
+        pattern += row + (i !== n ? "\n" : '')
+    }
+    return pattern
+}
+
+console.log(rightAngledNumberIncrement(4))
+
+// Number Pattern - Right Angled Increment
+console.log("------ Number Pattern - Right Angled Incremental Y-axis ---------")
+
+function rightAngledIncrementYAxis(n){
+    let pattern = ''
+    for(let i=1;i<=n;i++){
+        let row = ''
+        for(let j=1;j<=i;j++){
+            row += i
+        }
+        pattern += row + (i !== n ? "\n" : '')
+    }
+    return pattern
+}
+
+console.log(rightAngledIncrementYAxis(4))
