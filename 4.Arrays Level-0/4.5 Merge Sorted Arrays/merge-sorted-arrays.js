5b0047373257535ffc08baeaff1d110371b71178
Sandeep <g.sandeepreddy98@gmail.com>
2025-07-01 00:05:29 +0530

    feat(4. Arrays Level-0): 4.5 Merged Sorted Array
    Created a function to merge sorted arrays without using extra space.

--- 4.Arrays Level-0/4.5 Merge Sorted Arrays/merge-sorted-arrays.js
+++ 4.Arrays Level-0/4.5 Merge Sorted Arrays/merge-sorted-arrays.js
@@ -5,6 +5,8 @@
  * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
  * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
+
+// Brute force - approach - O((m+n)log(m+n))
 function mergeSortedArrayBruteForce(nums1, m, nums2, n){
     nums1 = [...nums1.slice(0,m),...nums2]
     return nums1.sort((a,b) => a-b)
@@ -14,7 +16,8 @@ console.log(mergeSortedArrayBruteForce([1,2,3,0,0,0],3,[2,5,6],3))
 console.log(mergeSortedArrayBruteForce([1],1,[],0))
 console.log(mergeSortedArrayBruteForce([2,0],1,[1],1))
 
-function mergeSortedArray(nums1, m, nums2, n){
+// Utilising an extra space - Time Complexity :- O(m+n) , Space Complexity = O(m)
+function mergeSortedArrayWithExtraSpace(nums1, m, nums2, n){
     const num1Copy = nums1.slice(0,m)
     let pointer1 = 0
     let pointer2 = 0
@@ -30,6 +33,28 @@ function mergeSortedArray(nums1, m, nums2, n){
     return nums1
 }
 
-console.log(mergeSortedArray([1,2,3,0,0,0],3,[2,5,6],3))
-console.log(mergeSortedArray([1],1,[],0))
-console.log(mergeSortedArray([2,0],1,[1],1))
+console.log(mergeSortedArrayWithExtraSpace([1,2,3,0,0,0],3,[2,5,6],3))
+console.log(mergeSortedArrayWithExtraSpace([1],1,[],0))
+console.log(mergeSortedArrayWithExtraSpace([2,0],1,[1],1))
+
+// Merging sorted array in place - Time Complexity :- O(m+n) , No extra space
+function mergeSortedArray(nums1, m, nums2, n){
+    let p1 = m-1
+    let p2 = n-1
+    for(let i = m+n-1 ;i >=0;i--){
+        if(p2 < 0){
+            break
+        }
+        if(p1 >= 0 && nums1[p1] > nums2[p2]){
+            nums1[i] = nums1[p1]
+            p1--
+        }else{
+            nums1[i] = nums2[p2]
+            p2--
+        }
+    }
+}
+
+console.log(mergeSortedArrayWithExtraSpace([1,2,3,0,0,0],3,[2,5,6],3))
+console.log(mergeSortedArrayWithExtraSpace([1],1,[],0))
+console.log(mergeSortedArrayWithExtraSpace([2,0],1,[1],1))
