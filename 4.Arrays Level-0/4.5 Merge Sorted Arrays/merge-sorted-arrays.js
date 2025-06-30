3a004aff244ad548f8611a4b6d5bb6acd3c1e0f6
Sandeep <g.sandeepreddy98@gmail.com>
2025-06-30 23:49:51 +0530

    feat(4. Arrays Level-0): 4.5 Merge sorted Array
    Created a funcion that merge sorted array by utilising an extra sapce for `num1Copy`.

--- 4.Arrays Level-0/4.5 Merge Sorted Arrays/merge-sorted-arrays.js
+++ 4.Arrays Level-0/4.5 Merge Sorted Arrays/merge-sorted-arrays.js
@@ -13,3 +13,23 @@ function mergeSortedArrayBruteForce(nums1, m, nums2, n){
 console.log(mergeSortedArrayBruteForce([1,2,3,0,0,0],3,[2,5,6],3))
 console.log(mergeSortedArrayBruteForce([1],1,[],0))
 console.log(mergeSortedArrayBruteForce([2,0],1,[1],1))
+
+function mergeSortedArray(nums1, m, nums2, n){
+    const num1Copy = nums1.slice(0,m)
+    let pointer1 = 0
+    let pointer2 = 0
+    for(let i=0;i < nums1.length;i++){
+        if((pointer1 < m && num1Copy[pointer1] < nums2[pointer2]) || pointer2 >= n){
+            nums1[i] = num1Copy[pointer1]
+            pointer1++
+        }else if(nums2.length){
+            nums1[i] = nums2[pointer2]
+            pointer2++
+        }
+    }
+    return nums1
+}
+
+console.log(mergeSortedArray([1,2,3,0,0,0],3,[2,5,6],3))
+console.log(mergeSortedArray([1],1,[],0))
+console.log(mergeSortedArray([2,0],1,[1],1))
