5ed07b133cba749ca434f59a5c6b7ad386d7405a
Sandeep Reddy <52911183+Sandeepreddy98@users.noreply.github.com>
2026-07-07 22:01:45 +0530

    feat(4. Arrays Level-0): 4.6 Move Zeros
    Created a function to merge sorted arrays without using extra space.

---
+++ 4.Arrays Level-0/4.6 Move Zeroes/move-zeros.js
@@ -0,0 +1,29 @@
+// 283. Move Zeroes
+
+/**Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+  
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+**/
+
+var moveZeroes = function(nums) {
+    let pointer = 0
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] != 0){
+            nums[pointer] = nums[i]
+            pointer++
+        }
+    }
+
+    for(let i= pointer ;i<nums.length;i++){
+        nums[i] = 0
+    }
+};
+
+console.log(moveZeroes([0,1,0,3,12]))
