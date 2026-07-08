e734c9bca52071ff4841c9ee29352bf4d5cf0289
Sandeep Reddy <52911183+Sandeepreddy98@users.noreply.github.com>
2026-07-08 21:54:10 +0530

    feat(4. Arrays Level-0): 4.8 Missing Numbers
    Created a function to find out missing number in an array

---
+++ 4.Arrays Level-0/4.8 Missing Number/missing-number.js
@@ -0,0 +1,30 @@
+// 268. Missing Number
+
+/**
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [0,1]
+Output: 2
+Explanation:
+n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+**/
+
+var missingNumber = function(nums) {
+    let n = nums.length
+    let sumOfNumbers = (n * (n+1)) / 2
+    console.log(sumOfNumbers)
+    let sumOfElements = 0
+    for(let i=0;i<nums.length;i++){
+        sumOfElements = sumOfElements + nums[i]
+    }
+    return sumOfNumbers - sumOfElements
+};
+
+console.log(missingNumber([3,0,1]))
