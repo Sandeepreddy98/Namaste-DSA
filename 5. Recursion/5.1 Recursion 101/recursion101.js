92649ff59d11082cfcc052e4db313100428e1f63
Sandeep Reddy <52911183+Sandeepreddy98@users.noreply.github.com>
2026-07-09 22:18:07 +0530

    feat(5. Recursion): 5.1 Recursion 101
    Wrote a function that prints number from n to 1.

--- 5. Recursion/5.1 Recursion 101/recursion101.js
+++ 5. Recursion/5.1 Recursion 101/recursion101.js
@@ -11,3 +11,12 @@ Real life examples:
 3) Organisational Hirerachy
 **/
 
+// print numbers from n...1
+function print(num){
+  if(num == 0 ) return
+  console.log(num)
+  num = num-1
+  print(num)
+}
+
+print(5)
