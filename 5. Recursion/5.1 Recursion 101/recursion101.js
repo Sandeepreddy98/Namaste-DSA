687841b0a8f5a312e73326ad6c53914a545714bd
Sandeep Reddy <52911183+Sandeepreddy98@users.noreply.github.com>
2026-07-09 22:14:58 +0530

    feat(5. Recursion): 5.1 Recursion 101
    Wrote defintion of recursion, parts & real life examples

---
+++ 5. Recursion/5.1 Recursion 101/recursion101.js
@@ -0,0 +1,13 @@
+/**
+Recursion : A function calls itself to solve smaller version of the same problem.
+
+2 parts of recursion:
+1) Base Case : Stop condition (When to stop recursion)
+2) Recursive Case : Part where function calls itself
+
+Real life examples:
+1) Queue of people
+2) Comment threads (Reddit)
+3) Organisational Hirerachy
+**/
+
